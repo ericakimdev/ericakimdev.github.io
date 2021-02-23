8d333ef5a5216b24c8d6452f17b14670560d1629
Erica <orange7961@gmail.com>
2021-02-23 02:29:01 -0500

    Updates

---
+++ precache-manifest.a601ad301158777ca5caa3289971c557.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d6951ea340f4a59b1808ddda82b317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8364e6fbda59e3583e53",
+    "url": "/static/css/main.f2b4d780.chunk.css"
+  },
+  {
+    "revision": "32ec95c39d0c21449750",
+    "url": "/static/js/2.51fbe5ba.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.51fbe5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8364e6fbda59e3583e53",
+    "url": "/static/js/main.0ab58119.chunk.js"
+  },
+  {
+    "revision": "a804be6422caa4c4f97d",
+    "url": "/static/js/runtime-main.c33fec53.js"
+  },
+  {
+    "revision": "296be820da5d1668b5f47dc293865827",
+    "url": "/static/media/buckland.296be820.png"
+  },
+  {
+    "revision": "f78ee3d2ae47c7bf065ae89cd177aa3b",
+    "url": "/static/media/ericakim_photo2.f78ee3d2.jpg"
+  },
+  {
+    "revision": "3466565b593ea77013d28dd0a691eb08",
+    "url": "/static/media/morningstart.3466565b.png"
+  },
+  {
+    "revision": "e1327a9d1c8480a64a59a18bf5f1697e",
+    "url": "/static/media/pulse.e1327a9d.png"
+  }
+]);
